2cb3761662ed07c63a004da4c6f21f821e5248d5
DESKTOP-OMFT137\Remus <remusroman99@yahoo.com>
2022-05-18 18:00:25 +0300

    AppSettings

---
+++ E-LearningRecSysAPI/RecSysApi/Settings/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using RecSysApi.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecSysApi.Presentation.Settings
+{
+    public class AppSettings : IAppSettings
+    {
+        public string Secret { get; set; }
+        public string DbConnectionString { get; set; }
+    }
+}
